db2860523d88a37195ccd3d948ba7054e76583ac
Jjjunior7 <herbert18h@gmail.com>
2023-10-03 12:15:24 -0300

    :sparkles: feat: Hero Section

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import React from "react";
 import styles from "./header.module.scss";
 import Themes from "../themes";
 import Navigation from "../navigation";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 function Header() {
+  const { scrollY } = useScroll();
+  const [hidden, setHidden] = React.useState(false);
+
+  useMotionValueEvent(scrollY, "change", (y: number) => {
+    const previus = scrollY.getPrevious();
+    if (y > previus && y > 150) setHidden(true);
+    else {
+      setHidden(false);
+    }
+  });
+
   return (
-    <header className={styles.container}>
+    <motion.header
+      className={styles.container}
+      variants={{
+        visible: { y: 0 },
+        hidden: { y: "-100%" },
+      }}
+      initial="hidden"
+      animate={hidden ? "hidden" : "visible"}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+    >
       <Themes />
       <Navigation />
-    </header>
+    </motion.header>
   );
 }
 
--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -4,10 +4,28 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <section className={styles.hero}>
-      <span className={styles.hero__link}>
-        <Link href={"/sobre"}>Olá, herbert por aqui!</Link>
-      </span>
+    <section className={styles.container} id="#welcome">
+      <article className={styles.hero__article__title}>
+        <Link
+          href="/sobre-mim"
+          title="Ir para sobre mim"
+          className={styles.hero__link}
+        >
+          <span>Olá, Herbert por aqui!</span>
+        </Link>
+        <h1 className={styles.hero__title}>Desenvolvedor</h1>
+      </article>
+      <article className={styles.hero__article__subtitle}>
+        <h1 className={styles.hero__title}>
+          Front-end<span>.</span>
+        </h1>
+        <p className={styles.hero__subtitle}>
+          Foco em criar experiências digitais para todos.
+        </p>
+      </article>
+      <Link href="#resume" title="Ir para resumo">
+        <span className={styles.arrow}></span>
+      </Link>
     </section>
   );
 }
